5c6cead861aec866fd6277e0cc41b197296c4262
gdunghi <gdunghi@gmail.com>
2016-12-18 15:10:02 +0700

    login css

--- src/main/java/com.account.config/WebSecurityConfig.java
+++ src/main/java/com.account.config/WebSecurityConfig.java
@@ -30,17 +30,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 
 	  http.authorizeRequests()
-		.antMatchers("/index").access("hasRole('ROLE_ADMIN')")		
-		.anyRequest().permitAll()
+		//.antMatchers("/index").access("hasRole('ROLE_ADMIN')")		
+			.anyRequest().authenticated()
 		.and()
-		  .formLogin().loginPage("/login")
-		  .usernameParameter("username").passwordParameter("password")
+		  .formLogin()
+		  	.loginPage("/login")
+		  	.usernameParameter("username").passwordParameter("password")
+		  	.permitAll()
 		.and()
 		  .logout().logoutSuccessUrl("/login?logout")	
 		 .and()
 		 .exceptionHandling().accessDeniedPage("/403")
 		.and()
-		  .csrf();
+		  .csrf(); 
+
+		   
+ 
 	}
 	
 }
