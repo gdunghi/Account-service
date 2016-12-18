a26c95c051d03a24c7329a8de18621343ffdd266
gdunghi <gdunghi@gmail.com>
2016-12-19 01:25:51 +0700

    example db

--- src/example_db.sql
+++ src/example_db.sql
@@ -1,9 +1,14 @@
 
-CREATE  TABLE users (
-  username VARCHAR(45) NOT NULL ,
-  password VARCHAR(45) NOT NULL ,
-  enabled TINYINT NOT NULL DEFAULT 1 ,
-  PRIMARY KEY (username));
+CREATE TABLE `users` (
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `enabled` tinyint(4) NOT NULL DEFAULT '1',
+  `fname` varchar(45) DEFAULT NULL,
+  `lname` varchar(45) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 CREATE TABLE user_roles (
   user_role_id int(11) NOT NULL AUTO_INCREMENT,
@@ -15,9 +20,9 @@ CREATE TABLE user_roles (
   CONSTRAINT fk_username FOREIGN KEY (username) REFERENCES users (username));
 
 INSERT INTO users(username,password,enabled)
-VALUES ('admin','123456', true);
+VALUES ('admin','123456', true,'jirayu','a','jirayu.a@gmail.com');
 INSERT INTO users(username,password,enabled)
-VALUES ('user','123456', true);
+VALUES ('user','123456', true,'kitichet','b','kitichet.a@gmail.com');
 
 INSERT INTO user_roles (username, role)
 VALUES ('admin', 'ROLE_USER');
@@ -26,4 +31,4 @@ VALUES ('admin', 'ROLE_ADMIN');
 INSERT INTO user_roles (username, role)
 VALUES ('user', 'ROLE_USER');
 
-select username,password, enabled from users where username= 'admin'
+ 
